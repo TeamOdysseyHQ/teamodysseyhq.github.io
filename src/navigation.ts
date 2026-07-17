eaa3577339bfe610a0fd685e7ade63dc575d978e
Csral <me@csral.dev>
2026-07-18 02:41:58 +0530

    Clean Records page

--- src/navigation.ts
+++ src/navigation.ts
@@ -1,32 +1,6 @@
-import { getAsset } from './utils/permalinks';
-
-export const headerData = {
-  links: [
-    {
-      text: 'Home',
-      href: '/',
-    },
-    {
-      text: 'Team',
-      href: '/team',
-    },
-    {
-      text: 'About',
-      href: '/about',
-    },
-    {
-      text: 'Blog',
-      href: '/blog',
-    },
-    {
-      text: 'Links',
-      href: '/links',
-    },
-  ],
-};
-
 export const footerData = {
-  links: [{}, {}, {}, {}],
+  links: [],
+  secondaryLinks: [],
   socialLinks: [
     { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: 'https://www.instagram.com/teamodysseyhq' },
     {
@@ -34,7 +8,6 @@ export const footerData = {
       icon: 'tabler:brand-linkedin',
       href: 'https://www.linkedin.com/in/avv-b-rover-team/',
     },
-    { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
     { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/TeamOdysseyHQ/' },
   ],
 };
